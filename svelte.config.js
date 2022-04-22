9a85f7482faae4bb59a019b0cbdb67a70b08ac5f
Nathan Eads <eadsnf@gmail.com>
2022-04-22 00:46:43 -0500

    Init

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/your-repo-name",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+    }
 };
 
 export default config;
