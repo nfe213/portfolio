37197539c0b11417618adf33874f099db976901f
Nathan Eads <eadsnf@gmail.com>
2022-04-26 10:23:06 -0500

    Vercel Test

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,46 @@
-import adapter from "@sveltejs/adapter-static"; 
-// was "@sveltejs/adapter-auto"
+import vercel from '@sveltejs/adapter-vercel';
 
-const dev = "production" === "development";
+export default {
+	kit: {
+		// default options are shown
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
 
-/** @type {import(""@sveltejs/kit").Config} */
-const config = {
-    kit: {
-        prerender: {
-            default: true
-        },
-        adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-        paths: {
-            // change below to your repo name
-            base: dev ? "/portfolio" : "/portfolio",
-        }
-    }
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
+	}
 };
 
-export default config;
+// GitHub Release
+// import adapter from "@sveltejs/adapter-static"; 
+// // was "@sveltejs/adapter-auto"
+
+// const dev = "production" === "development";
+
+// /** @type {import(""@sveltejs/kit").Config} */
+// const config = {
+//     kit: {
+//         prerender: {
+//             default: true
+//         },
+//         adapter: adapter({
+//             pages: "docs",
+//             assets: "docs"
+//         }),
+//         paths: {
+//             // change below to your repo name
+//             base: dev ? "/portfolio" : "/portfolio",
+//         }
+//     }
+// };
+
+// export default config;
